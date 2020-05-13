dadef11f3ad544f2d167d36ea6e13cb7840f5bb0
thomas dubiel <github email address>
2020-05-12 22:14:44 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class UsersController < ApplicationController
   def index
     @users = User.all
@@ -7,9 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.valid?
-      @user.save
+    #raise params.inspect
+    @user = User.create(user_params)
+    #binding.pry
+    if @user.save
       session[:user_id] = @user.id
       redirect_to :welcome
     else
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def welcome
-    @user = User.find(params[:id]) if @user
+    @user = User.find(session[:user_id]) 
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   get '/welcome', to: "users#welcome"
-  delete 'logout', to: "sessions#destroy"
+  get '/logout', to: "sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
